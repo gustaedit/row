f0e5a2973ad92bf16dae3e9d28a3f5fa41855446
gustaedit <gustaaa.edit@gmail.com>
2023-12-19 12:35:13 -0300

    desafio pt2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,107 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Row {
+  id: number;
+  name: string;
+  img?: string;
 }
+
+interface Response {
+  success: boolean;
+  data: Row[];
+}
+
+const ListaOlimpiadas: React.FC = () => {
+  const [rows, setRows] = useState<Row[]>([]);
+  const [playingVideoId, setPlayingVideoId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<Response>("https://app.olimpiadas.app/teste");
+        setRows(response.data.data);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const getMediaType = (url: string | undefined): "image" | "video" | "site" => {
+    if (!url) {
+      return "site";
+    }
+
+    if (url.toLowerCase().endsWith(".jpeg") || url.toLowerCase().endsWith(".jpg")) {
+      return "image";
+    }
+
+    if (url.includes("youtube.com")) {
+      return "video";
+    }
+
+    return "site";
+  };
+
+  const extractVideoId = (url: string | undefined): string | undefined =>
+    url &&
+    (url.match(/(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/)?.[1] ||
+      url.match(/i3\.ytimg\.com\/vi\/\s*([a-zA-Z0-9_-]+)\s*\/maxresdefault\.jpg/)?.[1]);
+
+  const generateThumbnailUrl = (videoId: string | undefined): string | undefined =>
+    videoId && `https://i3.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
+
+  const handleThumbnailClick = (videoId: string) => setPlayingVideoId(videoId);
+  const handleVideoEnd = () => setPlayingVideoId(null);
+
+  return (
+    <div style={{ maxWidth: "800px", margin: "0 auto" }}>
+      <h1 style={{ textAlign: "center" }}>Lista de Conteúdos</h1>
+      {rows.length === 0 && <p style={{ textAlign: "center" }}>Carregando...</p>}
+      {rows.map((row) => (
+        <div key={row.id} style={{ margin: "10px", padding: "10px", border: "1px solid #ddd", borderRadius: "8px" }}>
+          <h2>{row.name}</h2>
+          {row.img && !playingVideoId && (
+            <>
+              {getMediaType(row.img) === "image" && (
+                <img
+                  src={row.img}
+                  alt={row.name}
+                  style={{ width: "100%", height: "auto", display: "block", margin: "0 auto", cursor: "pointer" }}
+                />
+              )}
+              {getMediaType(row.img) === "video" && (
+                <img
+                  src={generateThumbnailUrl(extractVideoId(row.img))}
+                  alt={row.name}
+                  style={{ width: "100%", height: "auto", display: "block", margin: "0 auto", cursor: "pointer" }}
+                  onClick={() => handleThumbnailClick(extractVideoId(row.img) || "")}
+                />
+              )}
+              {getMediaType(row.img) === "site" && (
+                <a href={row.img} target="_blank" rel="noopener noreferrer">
+                  Visitar Site
+                </a>
+              )}
+            </>
+          )}
+          {playingVideoId === extractVideoId(row.img) && (
+            <iframe
+              src={row.img}
+              title={row.name}
+              width="100%"
+              height="300"
+              style={{ display: "block", margin: "0 auto" }}
+              allowFullScreen
+              onEnded={handleVideoEnd}
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ListaOlimpiadas;
